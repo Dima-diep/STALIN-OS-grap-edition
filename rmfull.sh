07dc2fcb09324bda4603d784c08f417f7d245470
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:45:55 +0300

    rmfull.sh

---
+++ rmfull.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+IYP=$(whiptail --title "Remove your file/directory" --inputbox "Write full path to file/directory" 10 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    clear
+    shred -n 10 $IYP
+else
+    clear
+    python3 /data/data/com.termux/files/chroot/chroot.py
+fi
