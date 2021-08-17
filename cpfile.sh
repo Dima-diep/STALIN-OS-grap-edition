d45fa77c9ec04ec7b6262ee27dcc951e37053ed2
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:20:03 +0300

    cpfile.sh

---
+++ cpfile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+IYP=$(whiptail --title "Copy your file" --inputbox "Path to file" 10 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    clear
+    YIP=$(whiptail --title "Copy your file" --inputbox "Path to directory" 10 60 3>&1 1>&2 2>&3)
+
+    exitstatus=$?
+
+    if [ $exitstatus = 0 ]; then
+        clear
+        cp $IYP $YIP
+        sleep 5
+    else
+        clear
+        python3 /data/data/com.termux/files/chroot/chroot.py
+    fi
+else
+    clear
+    python3 /data/data/com.termux/files/chroot/chroot.py
+fi
