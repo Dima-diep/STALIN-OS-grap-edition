70f277d7a79ff2e05d47a89b1a5452a4f1926834
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:46:41 +0300

    rubyrun.sh

---
+++ rubyrun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+IYP=$(whiptail --title "Run script" --inputbox "Write full path to script" 10 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    clear
+    ruby $IYP
+    sleep 5
+else
+    clear
+    python3 /data/data/com.termux/files/chroot/chroot.py
+fi
