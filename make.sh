34ed778ed449981fbe4dd024ef705efde21dcc6e
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:36:13 +0300

    make.sh

---
+++ make.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+IYP=$(whiptail --title "Makefile" --inputbox "Write your directory" 10 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    clear
+    cd $IYP
+    YIP=$(whiptail --title "Makefile" --inputbox "Write filename (without .c/.cpp), Makefile or parameters" 10 60 3>&1 1>&2 2>&3)
+
+    exitstatus=$?
+
+    if [ $exitstatus = 0]; then
+        clear
+        make $YIP
+    else
+        clear
+        python3 /data/data/com.termux/files/chroot/chroot.py
+    fi
+else
+    clear
+    python3 /data/data/com.termux/files/chroot/chroot.py
+fi
