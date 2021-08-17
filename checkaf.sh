7fc0c8523f12acc7cdf3af592d54b3a70e8f3474
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:16:36 +0300

    checkaf.sh

---
+++ checkaf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+IYP=$(whiptail --title "Check files" --inputbox "Write your directory (full path)" 10 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    clear
+    ls -al $IYP
+    sleep 10
+    clear
+else
+    clear
+    python3 /data/data/com.termux/files/chroot/chroot.py
+fi
