d57e2079d0e52dd608d2e328ef14e31754887683
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:21:25 +0300

    geomac.py

---
+++ geomac.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+import os
+
+try:
+    while True:
+        os.system("bash /data/data/com.termux/files/system/geomac.sh")
+except KeyboardInterrupt:
+    os.system("python3 /data/data/com.termux/files/system/geomac.py")
