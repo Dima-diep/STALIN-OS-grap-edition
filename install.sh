ec4f53e447e27a8b2aa3f6a884ccf005b634bdc9
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:25:08 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+apt install python3 mpv termux-api htop-legacy tor libc++ tar wget calc openssh man mc -yq
+apt install telegram-cli -yq
+python3 -m pip install --upgrade pip
+python3 -m pip install termcolor requests tqdm PTable urllib3 charset-normalizer certifi idna
+termux-setup-storage
+apt remove zsh -yq
+apt autoremove -yq
+mv /data/data/com.termux/files/home/.bashrc /data/data/com.termux/files/home/.bashrc.old
+cd /data/data/com.termux/files
+mkdir boot
+mkdir chroot
+mkdir system
+mkdir .initialize
+cd boot
+mkdir bootanimation
+cd ../home/STALIN-OS-grap-edition
+mv boot.py ../../boot
+mv boot.tar.gz ../../boot/bootanimation
+mv *.mp3 ../../boot
+mv grub.* ../../boot
+mv chroot* ../../chroot
+mv check* ../../chroot
+mv pacman* ../../chroot
+mv git* ../../chroot
+mv iyp.sh ../../chroot
+mv crepack.sh ../../chroot
+mv pip* ../../chroot
+mv ruby* ../../chroot
+mv exit.sh ../../chroot
+mv login.py ../../boot
+mv pass.py ../../boot
+mv chlogin.py ../../chroot
+mv chpass.py ../../chroot
+mv make.sh ../../chroot
+mv contacts.py ../../chroot
+mv game.sh ../../chroot
+mv atilo ../../usr/bin
+mv findfile.sh ../../chroot
+mv python* ../../chroot
+mv compile.sh ../../chroot
+mv golang.sh ../../chroot
+mv java* ../../chroot
+mv php.sh ../../chroot
+mv rust.sh ../../chroot
+mv mkdir.sh ../../chroot
+mv mkfile.sh ../../chroot
+mv rm* ../../chroot
+mv move.sh ../../chroot
+mv cp* ../../chroot
+mv chmodattr.sh ../../chroot
+mv terminal* ../../system
+mv netcat* ../../system
+mv zenmap.py ../../system
+mv apache* ../../system
+mv menu* ../../system
+mv uninstall.py ../../system
+mv proot* ../../system
+mv atilo-in.py ../../system
+mv atilo-r.py ../../system
+mv atilo-del* ../../system
+mv openapp* ../../chroot
+mv ofte.py ../../chroot
+mv ssh* ../../chroot
+cd ../
+git clone https://github.com/Dima-diep/geomac
+cd geomac
+chmod 777 *
+bash install.sh
+cd ../
+git clone https://github.com/Dima-diep/Music-Termux
+cd Music-Termux
+chmod 777 *
+cd ../
+git clone https://github.com/Dima-diep/zenmap
+cd zenmap
+chmod 777 *
+bash install.sh
+cd ../
