d4e9bcbb499e6b301f678ddbdcb4dcc5d93fb8d2
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:35:18 +0300

    java.sh

---
+++ java.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+IYP=$(whiptail --title "Run your program" --inputbox "Write your directory" 10 60 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    clear
+    cd $IYP
+    YIP=$(whiptail --title "Run your program" --inputbox "Write your program name" 10 60 3>&1 1>&2 2>&3)
+
+    exitstatus=$?
+
+    if [ $exitstatus = 0 ]; then
+        clear
+        java $YIP
+        sleep 5
+    else
+        clear
+        python3 /data/data/com.termux/files/chroot/chroot.py
+    fi
+else
+    clear
+    python3 /data/data/com.termux/files/chroot/chroot.py
+fi
