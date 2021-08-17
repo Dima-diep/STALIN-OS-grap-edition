683d115894f8e2b164ea4fda8727edc93c6d52bc
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:15:29 +0300

    atilo-in.py

---
+++ atilo-in.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+import os
+
+try:
+    os.system("clear && python3 /data/data/com.termux/files/usr/bin/atilo images")
+    print("Select your system:")
+    a = input()
+    os.system("python3 /data/data/com.termux/files/usr/bin/atilo pull " + a)
+    os.system("python3 /data/data/com.termux/files/usr/bin/atilo run " + a)
+except KeyboardInterrupt:
+    os.system("python3 /data/data/com.termux/files/boot/atilo-in.py")
