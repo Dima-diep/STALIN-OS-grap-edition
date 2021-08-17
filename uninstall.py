01e84ea68efbfbd5b38c3e7f510b62a4cd28ee8a
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-18 00:24:52 +0300

    uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -16,7 +16,6 @@ try:
         os.system("echo \"#!/bin/bash\" > /data/data/com.termux/files/usr/bin/login")
         os.system("echo \"cat ../usr/etc/motd\" >> /data/data/com.termux/files/usr/bin/login")
         os.system("echo \"bash\" >> /data/data/com.termux/files/usr/bin/login")
-        os.system("bash")
     elif a == 'no':
         os.system("python3 /data/data/com.termux/files/system/terminal.py")
 except KeyboardInterrupt:
