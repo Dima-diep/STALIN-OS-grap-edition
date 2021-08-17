cb2a95d3672ce0dab0376f4268de7fe8688fdc94
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-17 20:44:59 +0300

    proot-in.py

---
+++ proot-in.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+import os
+
+try:
+    os.system("clear && proot-distro list")
+    print("Select your system:")
+    a = input()
+    os.system("proot-distro install " + a)
+    os.system("proot-distro login " + a)
+except KeyboardInterrupt:
+    os.system("python3 /data/data/com.termux/files/boot/proot-in.py")
