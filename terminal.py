244a71f2b27aaf4dac4b2e8447533bb84b84563b
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-18 00:14:20 +0300

    terminal.py

--- terminal.py
+++ terminal.py
@@ -3,7 +3,6 @@
 import os
 
 try:
-    while True:
-        os.system("bash /data/data/com.termux/files/system/terminal.sh")
+    os.system("bash /data/data/com.termux/files/system/terminal.sh")
 except KeyboardInterrupt:
     os.system("python3 /data/data/com.termux/files/system/terminal.py")
