2ca3f1d28df3f777720d2c778e7013dc86b81513
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-25 16:08:26 +0400

    uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -16,6 +16,7 @@ try:
         os.system("echo \"#!/bin/bash\" > /data/data/com.termux/files/usr/bin/login")
         os.system("echo \"cat ../usr/etc/motd\" >> /data/data/com.termux/files/usr/bin/login")
         os.system("echo \"bash\" >> /data/data/com.termux/files/usr/bin/login")
+        os.system("rm -rf /data/data/com.termux/files/home/STALIN-OS-grap-edition")
     elif a == 'no':
         os.system("python3 /data/data/com.termux/files/system/terminal.py")
 except KeyboardInterrupt:
